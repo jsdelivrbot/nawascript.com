c472612a066fbe117ca3a716317702b45b57d506
Paul Borawski <paulborawski14@gmail.com>
2016-04-02 11:00:36 -0700

    added photo

--- public/index.js
+++ public/index.js
@@ -1,1 +1,11 @@
+document.getElementById("one").style.color = "#777";
 
+var sdegree = 0;
+
+$(window).scroll(function() {
+
+    sdegree ++ ;
+    sdegree = sdegree + 2 ;
+    var srotate = "rotate(" + sdegree + "deg)";
+    $("img").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
+});
