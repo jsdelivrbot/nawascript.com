2588a4496c2bf811e16b18f16d4d92479d6d0b1c
Paul Borawski <paulborawski14@gmail.com>
2016-07-09 11:14:25 -0700

    WORKING REACT APP

--- nawascript.js
+++ nawascript.js
@@ -1,18 +1,65 @@
-var port = 30888;
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-var app = require('express')();
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
 
-app.use(require('compression')());
-app.use(require('body-parser').json());
-app.use(require('body-parser').urlencoded({ extended:true }));
-/*app.use(require('stylus').middleware(require('path').join(__dirname, 'public')));*/
-app.use(require('express').static('public'));
+app.set('port', (process.env.PORT || 30888));
 
-app.get('/', function (req, res) {
-  res.send('nawascript');
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Additional middleware which will set headers that we need on each request.
+app.use(function(req, res, next) {
+    // Set permissive CORS header - this allows this server to be used only as
+    // an API server in conjunction with something like webpack-dev-server.
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Disable caching so we'll always get the latest comments.
+    res.setHeader('Cache-Control', 'no-cache');
+    next();
+});
+
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
 });
 
 
-require('http').createServer(app).listen(port,function(){
-  console.log('server is running you beeezy, on port', port)
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
